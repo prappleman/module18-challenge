cd483718fb7cb8cdc6f1ff96aa152bd1c69f2b96
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-15 22:12:59 -0600

    fixed bugs with hosting on mongo

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,8 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI);
-
-// Log MongoDB URI
-console.log('MongoDB URI:', process.env.MONGODB_URI);
-
-// Event listeners to handle connection status
-mongoose.connection.on('connected', () => {
-  console.log('MongoDB connected successfully!');
+mongoose.connect('mongodb://localhost:27017/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
-
-// Use this to log mongo queries being executed!
-mongoose.set("debug", true);
 
 module.exports = mongoose.connection;
