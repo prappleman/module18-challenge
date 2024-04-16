ec85e6ad772bcc4c197a19dfa5df73fee923f21e
parkerrappleye <parker.rappleye1@gmail.com>
2024-04-15 20:34:37 -0600

    added an event listener

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-mongoose.connect(
-  process.env.MONGODB_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+
+// Log MongoDB URI
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+
+// Event listeners to handle connection status
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected successfully!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
